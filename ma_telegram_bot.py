a8fd1cdf1b0a79f653a16061bdbcb840520977c5
LuisAZambrana <37357460+LuisAZambrana@users.noreply.github.com>
2020-04-30 18:25:58 -0300

    Actualizacion de ma_telegram_bot.py
    Se comenta commands devido a que no funciona mas en python3. Se pasa a subprocess.

--- ma_telegram_bot.py
+++ ma_telegram_bot.py
@@ -5,16 +5,18 @@
 import telebot
 bot = telebot.TeleBot("aca pone tu token");
 
-import commands
+#import commands lo borramos por que a partir del python3 no va mas
 import requests
 import os
 import time
 import sys
 import RPi.GPIO as gpio
 import subprocess
+from subprocess import Popen, PIPE
 
 owner == tuid
 
+##le mandas la palabra 18on   y enciende el pin 18
 @bot.message_handler(func=lambda message: message.text.lower() == '18on' and message.chat.id == owner)
 def prende_18(message):
     gpio.setwarnings(False)
@@ -23,6 +25,7 @@ def prende_18(message):
     gpio.output(18, True)
     bot.send_message(message.chat.id, "Pin 18 Encendido");
 
+##le mandas la palabra 18off   y apaga el pin 18
 @bot.message_handler(func=lambda message: message.text.lower() == '18off'and message.chat.id == owner)
 def prende_18(message):
     gpio.setwarnings(False)
@@ -31,17 +34,20 @@ def prende_18(message):
     gpio.output(18, True)
     bot.send_message(message.chat.id, "Pin 18 Apagado");
 
+##Mandas la palabra temperatura y te devuelve la temperatura de la raspberry
 @bot.message_handler(func=lambda message: message.text.lower() == 'temperatura'and message.chat.id == owner)
 def temperatura(message):
     temp    = round(int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3, 2)
-    tempgpu = commands.getoutput('/opt/vc/bin/vcgencmd measure_temp').replace('temp=', '').replace("'C", '')
+    tempgpu = subprocess.getoutput('/opt/vc/bin/vcgencmd measure_temp').replace('temp=', '').replace("'C", '')
     bot.send_message(message.chat.id, "Temperatura:\nCPU: {} °C\nGPU: {} °C".format(temp, tempgpu));
 
+##Mandas la palabra uptime y te devuelve el tiempo en que esta funcionando la rasp
 @bot.message_handler(func=lambda message: message.text.lower() == 'uptime' and message.chat.id == owner)
 def temperatura(message):
-    uptime = commands.getoutput('uptime -p')
+    uptime = subprocess.getoutput('uptime -p')
     bot.send_message(message.chat.id, "Uptime: {}".format(uptime));
 
+#Mandas la palabra ip y te devuelvve la ip publica en donde se encuentra tu raspberry
 @bot.message_handler(func=lambda message: message.text.lower() == 'ip'and message.chat.id == owner)
 def miip(message):
     ip = requests.get("http://ipinfo.io/ip")
